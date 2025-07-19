2ff4f4c00151332f6a610791d4ec22000f0570a5
ViswaaE <viswaaeswaran@gmail.com>
2025-07-19 23:45:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// console.log("hello");
+// console.log("javascript practice");
+
+
+// window.alert("javascript practice");
+// window.alert("welcome");
+
+
+
+// document.getElementById("h1").textContent = `hello`;
+// document.getElementById("p").textContent = `paragraph tag`;
+
+
+// variables practice
