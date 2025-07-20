b31ff1dbc788394806061bb932c1e81f2ff32552
ViswaaE <viswaaeswaran@gmail.com>
2025-07-20 13:22:11 +0530

    operators

--- script.js
+++ script.js
@@ -12,3 +12,40 @@
 
 
 // variables practice
+// let fullname = "viswaa";
+// let age = 21;
+// let isStudent = true;
+
+// document.getElementById("name").textContent = `my name is ${fullname}`;
+// document.getElementById("age").textContent = `I'm ${fullname} years old`;
+// document.getElementById("student").textContent = `I'm a enrolled as student : ${isStudent}`;
+
+
+//operators practice
+
+let Students = 30;
+// Students = Students + 1;
+// Students = Students - 1;
+// Students = Students * 10;
+// Students = Students / 2;
+// Students = Students ** 15
+// Students = Students % 7;
+
+//augmented assingment operator
+// Students += 1;
+// Students -= 10;
+// Students *= 20;
+// Students /= 6;
+// Students **= 7;
+
+//incremenrt operator and decrement
+// Students ++;
+// Students --;
+
+// document.getElementById("students").textContent =`the total number of students are ${Students}`
+
+
+// How to accept user input
+
+// 1. EASY WAY = window prompt
+// 2. PROFESSIONAL WAY = HTML textbox
